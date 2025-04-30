6d32305a42fcc453672f02791bd942fff91c59f5
Alexandrbig1 <alexsmagin1@gmail.com>
2025-04-30 15:22:30 -0500

    fix: update

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
+import "dotenv/config";
 import moviesRouter from "./routes/moviesRouter.js";
 import morgan from "morgan";
 import rateLimit from "express-rate-limit";
-
-dotenv.config();
 
 const app = express();
 
